ae72f777d641cdf0e6087b337d97390ff8804b26
Teodor Taneski Feldschau <50484090+Teoslaf@users.noreply.github.com>
2023-04-20 17:11:00 +0200

    Creating a function to add a Node at the end

--- pushswap.c
+++ pushswap.c
@@ -6,7 +6,7 @@
 /*   By: ttaneski <ttaneski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 14:54:09 by ttaneski          #+#    #+#             */
-/*   Updated: 2023/04/20 16:39:05 by ttaneski         ###   ########.fr       */
+/*   Updated: 2023/04/20 17:10:33 by ttaneski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,37 @@ typedef struct Node
 	struct Node* next;	
 } Node;
 
+void insert_end(Node **root, int value)
+{
+	Node* new_node = malloc(sizeof(Node));
+	if(new_node == NULL)
+		exit(1);
+	new_node->next = NULL;
+	new_node->x = value;
+
+	Node *curr = *root;
+	while(curr->next != NULL)
+		curr = curr-> next;
+	curr->next = new_node;
+}
 int main(int argc, char **argv)
 {
-	Node root;
-	Node elem2; 
-	root.x = 15;
-	root.next = malloc(sizeof(Node));
-	root.next-> x = 2;
-	root.next-> next =NULL;
+	Node *root = malloc(sizeof(Node));
+	if(root == NULL)
+	{
+		exit(2);
+	}
+	root->x = 15;
+	root->next = NULL;
 
-	Node *curr = &root;
+	insert_end(&root, 10);
+	insert_end(&root, 11);
+	Node *curr = root;
 	while(curr != NULL)
 	{
 		printf("%d\n", curr->x);
 		curr = curr->next;
 	}
-	printf("first: %d\n", root.x);
-	printf("snd: %d\n", root.next->x);
 
-	free(root.next);
 	return(0);
 }
