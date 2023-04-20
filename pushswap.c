2dd7d7c39b89e652fe705f30298ce5865ca18860
Teodor Taneski Feldschau <50484090+Teoslaf@users.noreply.github.com>
2023-04-20 19:04:35 +0200

    creating a simple algoritam for sorting lists.

--- pushswap.c
+++ pushswap.c
@@ -6,50 +6,100 @@
 /*   By: ttaneski <ttaneski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 14:54:09 by ttaneski          #+#    #+#             */
-/*   Updated: 2023/04/20 17:10:33 by ttaneski         ###   ########.fr       */
+/*   Updated: 2023/04/20 19:04:00 by ttaneski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node 
+typedef struct Node
 {
 	int x;
-	struct Node* next;	
+	struct Node *next;
 } Node;
 
 void insert_end(Node **root, int value)
 {
-	Node* new_node = malloc(sizeof(Node));
-	if(new_node == NULL)
+	Node *new_node = malloc(sizeof(Node));
+	if (new_node == NULL)
 		exit(1);
 	new_node->next = NULL;
 	new_node->x = value;
 
+	if (*root == NULL)
+		*root = new_node;
 	Node *curr = *root;
-	while(curr->next != NULL)
-		curr = curr-> next;
+	while (curr->next != NULL)
+		curr = curr->next;
 	curr->next = new_node;
 }
+void insert_beginning(Node **root, int value)
+{
+	Node *new_node = malloc(sizeof(Node));
+	if (new_node == NULL)
+		exit(3);
+	new_node->x = value;
+	new_node->next = *root;
+
+	*root = new_node;
+}
+
+void insert_after(Node *node, int value)
+{
+	Node *new_node = malloc(sizeof(Node));
+	if (new_node == NULL)
+		exit(3);
+	new_node->x = value;
+	new_node->next = node->next;
+	node->next = new_node;
+}
+void deallocate(Node **root)
+{
+	Node *curr = *root;
+	while (curr != NULL)
+	{
+		Node *aux = curr;
+		curr = curr->next;
+		free(aux);
+	}
+	*root = NULL;
+}
+
+void insert_sorted(Node **root, int value)
+{
+	if (*root == NULL || (**root).x >= value)
+	{
+		insert_beginning(root, value);
+		return;
+	}
+	Node *curr = *root;
+	while (curr->next != NULL)
+	{
+		if (curr->next->x >= value)
+			break;
+		curr = curr->next;
+	}
+	insert_after(curr, value);
+}
+
 int main(int argc, char **argv)
 {
-	Node *root = malloc(sizeof(Node));
-	if(root == NULL)
-	{
-		exit(2);
-	}
-	root->x = 15;
-	root->next = NULL;
+	Node *root = NULL;
 
-	insert_end(&root, 10);
-	insert_end(&root, 11);
+	insert_sorted(&root, 4);
+	insert_sorted(&root, 2);
+	insert_sorted(&root, 9);
+	insert_sorted(&root, 1);
+	insert_sorted(&root, 3);
+
+
 	Node *curr = root;
-	while(curr != NULL)
+	while (curr != NULL)
 	{
 		printf("%d\n", curr->x);
 		curr = curr->next;
 	}
-
-	return(0);
+	deallocate(&root);
+	return (0);
 }
